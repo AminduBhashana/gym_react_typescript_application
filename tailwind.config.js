2216dfb1eb71ab32c54a3b49629df67a440b204c
AminduBhashana <waamidu@gmail.com>
2024-12-09 20:51:57 +0530

    basic configuration with tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,8 +20,23 @@ export default {
       backgroundImage: (theme) => ({
         "gradient-yellowred": "linear-gradient(90deg, #FF616A 0%, #FFc837 100%)",
         "mobile-home": "url('./assets/HomePageGraphic.png')"
-      })
+      }),
+      fontFamily: {
+        dmsans : ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat","sans-serif"]
+      },
+      content: {
+        evolvetext : "url('./assets/EvolveText.png')",
+        abstractWaves: "url('./assets/AbstractWaves.png')",
+        sparkles: "url('./assets/Sparkles.png')",
+        circles: "url('./assets/Circles.png')",
+      }
     },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px"
+    }
   },
   plugins: [],
 }

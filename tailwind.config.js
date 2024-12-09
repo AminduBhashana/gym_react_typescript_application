f0164db9e5cb610e54479f0611985ee16bead5a7
AminduBhashana <waamidu@gmail.com>
2024-12-09 20:42:17 +0530

    add images for assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,10 @@ export default {
         "secondary-400": "#FFCD58",
         "secondary-500": "FFC132",
       },
+      backgroundImage: (theme) => ({
+        "gradient-yellowred": "linear-gradient(90deg, #FF616A 0%, #FFc837 100%)",
+        "mobile-home": "url('./assets/HomePageGraphic.png')"
+      })
     },
   },
   plugins: [],

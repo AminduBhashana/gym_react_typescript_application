bf099900241af12cf176c5a77a21fa7c7496673f
AminduBhashana <waamidu@gmail.com>
2024-12-13 00:21:48 +0530

    complete the home page

--- src/shared/ActionButton.tsx
+++ src/shared/ActionButton.tsx
@@ -11,8 +11,8 @@ const ActionButton = ({children ,setSelectedPage}: Props) => {
   return (
     <AnchorLink
         className="rounded-md  bg-secondary-400 px-10 py-2 hover:bg-primary-500 hover:text-white"
-        onClick={() => setSelectedPage(SelectedPage.CobtactUs)}
-        href={`#${SelectedPage.CobtactUs}`}
+        onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+        href={`#${SelectedPage.ContactUs}`}
     >
         {children}
     </AnchorLink>

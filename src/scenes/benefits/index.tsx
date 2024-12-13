3812585a5dd5efa2051d5894d1e08e1c36dc3b13
AminduBhashana <waamidu@gmail.com>
2024-12-13 12:34:41 +0530

    complete benefits section

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -3,6 +3,8 @@ import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react
 import { motion } from "framer-motion";
 import HText from "../../shared/HText";
 import Benefit from "./Benefit";
+import ActionButton from "../../shared/ActionButton";
+import BenefitsPageGraphic from '../../assets/BenefitsPageGraphic.png';
 
 type Props = {
     setSelectedPage : (value: SelectedPage) => void;
@@ -75,6 +77,61 @@ const Benefits = ({setSelectedPage}: Props) => {
             />
           ))}
         </motion.div>
+        {/* GRAPHIC AND DESCRIPTION */}
+        <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+          {/* GRAPHIC */} 
+          <img 
+            className="mx-auto"
+            alt="benefits-page-graphic"
+            src={BenefitsPageGraphic}
+          />
+          {/* DESCRIPTION */}
+          <div>
+             {/* TITLE */}
+             <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-abstractWaves">
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{once: true, amount:0.5}}
+                  transition={{duration:0.5}}
+                  variants={{
+                      hidden : {opacity: 0, x: 50},
+                      visible : {opacity: 1, x: 0}
+                  }}
+                >
+                  <HText>MILLIONS OF HAPPY MEMBERS GETTING{" "}
+                    <span className="text-primary-500">FIT</span>
+                  </HText>
+                </motion.div>
+              </div>
+             </div>
+             {/* DESCRIPTION */}
+             <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{once: true, amount:0.5}}
+              transition={{delay:0.2, duration:0.5}}
+              variants={{
+                  hidden : {opacity: 0, x: -50},
+                  visible : {opacity: 1, x: 0}
+              }}
+             >
+                <p className="my-5">
+                Welcome to the ultimate gym experience! Our platform offers personalized workout plans, progress tracking, virtual coaching, and motivational resources to help you achieve your fitness goals. Whether you're at home or in the gym, stay on track with ease. Join today and take the first step toward a healthier, stronger you!
+                </p>
+                <p className="mb-5">
+                Elevate your fitness journey with our innovative gym platform. Access personalized workout plans, track your progress, and stay motivated anytime, anywhere. Start building a healthier, stronger you—because your fitness goals deserve the best!
+                </p>
+             </motion.div>
+             {/* BUTTON */}
+             <div className="relative mt-16">
+              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
+              </div>
+             </div>
+          </div>
+        </div>
       </motion.div>
     </section>
   )
